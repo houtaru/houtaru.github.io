a228573a9f7554dd4ae40735a4ed2be2a1066fee
San Cao <caothesan@gmail.com>
2026-05-16 00:21:10 +0700

    migrate from old to v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/v2',
   site: 'https://houtaru.github.io',
   integrations: [],
 });
